040287b464750731c17cca878596a80fbec42568
theIcedBlackTea <blacktemplehood@gmail.com>
2024-10-23 14:30:17 -0600

    Subida de index.js de la Práctica-09

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require(`express`); // Importar la libreria para crear un servidor web
+
+// Instanciar nuestra aplicación web
+const app = express()
+
+// Configuramos nuestro servidor web 
+const port = 3000;
+app.listen(port, ()=>{
+   console.log(`La aplicación ha iniciado en el puesto: ${port}`);
+})
+
+// Probamos las rutas para poder presentar mensajes al usuario a través del navegador
+app.get("/", function(req, res){
+    res.send("Hola Mundo desde Node, a través del Navegador")
+})
+
+app.get("/QuienSoy", function(req, res){
+    res.json({"estudiante": "Daniel de Jesús Bravo Godínez", 
+        "Carrera": "TI DSM",
+        "Grado": "4°",
+        "Grupo": "B",
+        "Asignatura": "Aplicaciones Web Orientada a Servicios (AWOS)"
+    })
+})
